cddd1bd0d83bd7f4891997f30950f7bf05bee979
PKC278 <52959804+PKC278@users.noreply.github.com>
2026-04-13 10:52:37 +0800

    my change

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -18,7 +18,6 @@ import {
   SETTINGS_MENU_KEY,
   TABLE_SIZE,
   TABLE_WIDTH_MODE,
-  TEST_URL,
   type THEME,
 } from '@/constant'
 import { getMinCardWidth, isMiddleScreen, isPreferredDark } from '@/helper/utils'
@@ -135,14 +134,14 @@ export const emoji = useStorage<EMOJIS>(
 export const customBackgroundURL = useStorage('config/custom-background-image', '')
 export const dashboardTransparent = useStorage('config/dashboard-transparent', 90)
 export const autoUpgradeDashboard = useStorage('config/auto-upgrade', false)
-export const checkUpgradeCore = useStorage('config/check-upgrade-core', true)
+export const checkUpgradeCore = computed(() => false)
 export const autoUpgradeCore = useStorage('config/auto-upgrade-core', false)
-export const swipeInPages = useStorage('config/swipe-in-pages', true)
+export const swipeInPages = computed(() => false)
 export const swipeInTabs = useStorage('config/swipe-in-tabs', false)
 export const disablePullToRefresh = useStorage('config/disable-pull-to-refresh', true)
 export const displayAllFeatures = useStorage('config/display-all-features', false)
 export const blurIntensity = useStorage('config/blur-intensity', 10)
-export const scrollAnimationEffect = useStorage('config/scroll-animation-effect', true)
+export const scrollAnimationEffect = computed(() => false)
 export const IPInfoAPI = useStorage('config/geoip-info-api', IP_INFO_API.IPSB)
 export const autoDisconnectIdleUDP = useStorage('config/auto-disconnect-idle-udp', false)
 export const autoDisconnectIdleUDPTime = useStorage('config/auto-disconnect-idle-udp-time', 300)
@@ -150,8 +149,8 @@ export const keyboardShortcuts = useStorage<Record<string, string>>('config/keyb
 
 // overview
 export const splitOverviewPage = useStorage('config/split-overview-page', false)
-export const autoIPCheck = useStorage('config/auto-ip-check', true)
-export const autoConnectionCheck = useStorage('config/auto-connection-check', true)
+export const autoIPCheck = computed(() => false)
+export const autoConnectionCheck = computed(() => false)
 export const showStatisticsWhenSidebarCollapsed = useStorage(
   'config/show-statistics-when-sidebar-collapsed',
   true,
@@ -213,7 +212,7 @@ export const proxyGroupFilterMap = useStorage<Record<string, string>>(
 )
 export const displayFinalOutbound = useStorage('config/show-selected-for-now-node', false)
 export const twoColumnProxyGroup = useStorage('config/two-columns', true)
-export const speedtestUrl = useStorage<string>('config/speedtest-url', TEST_URL)
+export const speedtestUrl = computed(() => 'http://cp.cloudflare.com')
 export const independentLatencyTest = useStorage('config/independent-latency-test', false)
 export const speedtestTimeout = useStorage<number>('config/speedtest-timeout', 5000)
 export const proxySortType = useStorage<PROXY_SORT_TYPE>(
@@ -241,7 +240,7 @@ export const minProxyCardWidth = useStorage<number>(
 )
 export const manageHiddenGroup = useStorage('config/manage-hidden-group-mode', false)
 
-export const displayGlobalByMode = useStorage('config/display-global-by-mode', false)
+export const displayGlobalByMode = computed(() => true)
 export const customGlobalNode = useStorage('config/custom-global-node-name', GLOBAL)
 
 export const proxyGroupIconSize = useStorage('config/proxy-group-icon-size', 24)
